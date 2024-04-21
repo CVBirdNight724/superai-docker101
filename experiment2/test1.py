b91a5d0a8c738826f3d31aefc0a46ec8bc0d520f
Witthawin <61728000+CVBirdNight724@users.noreply.github.com>
2024-04-21 09:06:59 +0700

    Not use experiment2/test1

--- experiment2/test1.py
+++ experiment2/test1.py
@@ -1,16 +1,17 @@
-import requests
+# ##### Not Use #####
+# import requests
 
-url = "http://127.0.0.1:5000/avg"
-json_body = {
-    'datas': [
-        100,
-        2000,
-        1.0,
-        2
-    ]
-}
+# url = "http://127.0.0.1:5000/avg"
+# json_body = {
+#     'datas': [
+#         100,
+#         2000,
+#         1.0,
+#         2
+#     ]
+# }
 
-response = requests.post(url, json=json_body)
+# response = requests.post(url, json=json_body)
 
-print(f'status : {response.status_code}')
-print(f'data: {response.json()}')
+# print(f'status : {response.status_code}')
+# print(f'data: {response.json()}')
